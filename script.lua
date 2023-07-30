c18adba698d125744c5a8fbc766615ecfbf8eb0d
ashimorigod <140102428+ashimorigod@users.noreply.github.com>
2023-07-31 02:51:47 +0300

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,13 @@
+script = {
+current_version = {
+       vip = "2.0",
+    },
+ 
+    key_check = {
+        vip = "LnUg2785Utmm56FX",
+    }, 
+
+    private_key = {
+        vip = "C828grnC38JvVM6c",
+    },
+}
